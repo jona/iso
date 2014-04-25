213bbef8cac392eaab396203c543b5557dbe37fe
Matthew Dodds <matthew@firma8.com>
2014-04-25 16:48:10 -0400

    Add 639-3 support

--- lib/iso/language.rb
+++ lib/iso/language.rb
@@ -1,5 +1,6 @@
 class ISO::Language < ISO::Subtag
-  DEFINITIONS_FILE          = "#{File.dirname(__FILE__)}/../../data/iso-639-1.yml"
+  DEFINITIONS_FILE          = "#{File.dirname(__FILE__)}/../../data/iso-639-3.yml"
+  MAPPING_FILE              = "#{File.dirname(__FILE__)}/../../data/iso-639-1-639-3-mapping.yml"
   DEFAULT_PLURAL_RULE_NAMES = %w(one other)
   DEFAULT_DIRECTION         = 'ltr'
   DEFAULT_CODE              = 'en'
@@ -13,9 +14,19 @@ class ISO::Language < ISO::Subtag
     super(code, options)
   end
 
+  def self.all_tags
+    @all_tags ||= YAML.load_file(DEFINITIONS_FILE).map do |code, options|
+      {code => options["name"]}
+    end
+  end
+
+  def self.convert_639_1_to_639_3(code)
+    YAML.load_file(MAPPING_FILE)[code] || code
+  end
+
   def self.identify(full_code)
     segments = full_code.split('-')
-    segments.first =~ /^([a-z]{2})$/ ? find($1) : nil
+    find convert_639_1_to_639_3 segments.first
   end
 
 private
