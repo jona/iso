425ec6b8be00e049da318785d9904ac1335851c0
Jonathan Morua <jonathan@firma8.com>
2014-05-13 18:05:48 -0400

    add methods for Tag object regularly used

--- lib/iso/tag.rb
+++ lib/iso/tag.rb
@@ -1,12 +1,26 @@
 module ISO
   class Tag
-    attr_reader :language, :regions, :code
+    attr_reader :language, :regions, :code, :original
 
     def initialize(code)
       language = ISO::Language.convert_639_1_to_639_3 code.scan(/^[A-Za-z]{1,}/).first
+      @original = code
       @code     = language
       @language = Language.identify(language)
-      @regions   = Region.identify(code) || UN::Region.identify(code)
+      @regions   = Region.identify(@original) || UN::Region.identify(@original)
     end
+
+    def normalize
+      self.language ? (self.language[:alpha2] ? self.language[:alpha2] : self.language[:code]) : self.original
+    end
+
+    def name
+      self.language ? (self.regions.empty? ? self.language[:name] : "#{self.language[:name]}-#{self.regions.join('-').upcase}") : self.original
+    end
+
+    def language_tag
+      self.regions.empty? ? self.normalize : "#{self.normalize}-#{self.regions.join('-')}"
+    end
+
   end
 end
